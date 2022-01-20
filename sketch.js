8e73051c09dcd01155cfb88854b55615b8892cfa
Pernixy <darkmastery27@gmail.com>
2022-01-20 17:43:14 -0600

    made this square thing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
   }
   
+console.log("Hello there");
+speed = 25;
+x = 200;
+wHalf = windowWidth/2;
+hHalf = windowHeight/2;
+
   function draw() {
-    background(220, 85, 60);
+    background(120, 185, 160);
+
+    //bottom right square
+    strokeWeight(5);
+    stroke(0);
+    fill(217, 201, 28);
+    rect(windowWidth/2, windowHeight/2, windowHeight);
+
+    //top left square
+    strokeWeight(5);
+    stroke(0);
+    fill(252, 152, 3);
+    rect(windowWidth/2, windowHeight/2, windowWidth * -1);
+
+    //bottom left square
+    strokeWeight(5);
+    stroke(0);
+    fill(51, 69, 166);
+    rect(0, windowHeight/2, windowWidth/2);
+
+    //top left square
+    strokeWeight(5);
+    stroke(0);
+    fill(93, 201, 62);
+    rect(windowWidth, windowHeight/2, -windowWidth/2);
+
+    x += speed;
+    if (x >= windowWidth - 25) {
+      speed = speed * -1;
+    }
+    if (x <= 0 + 25) {
+      speed = speed * -1;
+    }
   }
